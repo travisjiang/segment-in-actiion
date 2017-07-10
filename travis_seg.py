5204b93a409484576a21272c530eaee2f3fc9b9f
jiangtingyu <jiangtingyu@tp-link.com.cn>
2017-07-10 17:56:04 +0800

    first commit

---
+++ travis_seg.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+
+class Tokenizer(object):
+    def __init__(self, dictionary=DEFAULT_DICT):
+        self.freq_dict = {}
+
+    def get_DAG(self, sentence):
+        DAG = {}
+        N = len(sentence)
+        for k in range(N):
+            seg_list = [i for i in range(k, N-1) if sentence[k,i+1] in self.freq_dict]
+            DAG[k] = seg_list
+        return DAG
+
+    def __cut_all(self, sentence):
+        dag = self.get_DAG(sentence)
+        for k, L in items():
+            for i in L:
+                yeild sentence[k:i+1]
+
+    def calc(self, sentence, DAG, route):
+        N = len(sentence)
+        route[N] = (0,0)
+        for idx in range(N-1, -1, -1):
+            route[idx] = max((log(self.freq_dict.get(sentence[idx:x+1], 1)) + route[x+1][0], x) for x in DAG[idx])
+
+    def __cut_DAG_NO_HMM(self, sentence):
+        """
+        Without regard to english words in sentence
+        """
+        route = {}
+        dag = self.get_DAG(sentence)
+
+        self.calc(sentence, dag, route)
+
+        x = 0
+        while x < N:
+            y = route[x][1] + 1
+            yield sentence[x:y]
+            x = y
+
+    def __cut_DAG(self, sentence):
+        route = {}
+        dag = self.get_DAG(sentence)
+
+        self.calc(sentence, dag, route)
+
+        x = 0
+        while x < N:
+            y = route[x][1] + 1
+            yield sentence[x:y]
+            x = y
+
+
+    def calc(self, sentence, DAG, route):
+        N = len(sentence)
+        route[N] = (0, 0)
+        logtotal = log(self.total)
+        for idx in xrange(N - 1, -1, -1):
+            route[idx] = max((log(self.FREQ.get(sentence[idx:x + 1]) or 1) -
+                              logtotal + route[x + 1][0], x) for x in DAG[idx])
+
+    def get_DAG(self, sentence):
+        self.check_initialized()
+        DAG = {}
+        N = len(sentence)
+        for k in xrange(N):
+            tmplist = []
+            i = k
+            frag = sentence[k]
+            while i < N and frag in self.FREQ:
+                if self.FREQ[frag]:
+                    tmplist.append(i)
+                i += 1
+                frag = sentence[k:i + 1]
+            if not tmplist:
+                tmplist.append(k)
+            DAG[k] = tmplist
+            DAG[k] = [i for i in xrange(k,N-1) if sentence[k,i+1] in self.FREQ]
+        return DAG
+
+    def __cut_all(self, sentence):
+        dag = self.get_DAG(sentence)
+        old_j = -1
+        for k, L in iteritems(dag):
+            if len(L) == 1 and k > old_j:
+                yield sentence[k:L[0] + 1]
+                old_j = L[0]
+            else:
+                for j in L:
+                    if j > k:
+                        yield sentence[k:j + 1]
+                        old_j = j
+
+    def __cut_DAG_NO_HMM(self, sentence):
+        DAG = self.get_DAG(sentence)
+        route = {}
+        self.calc(sentence, DAG, route)
+        x = 0
+        N = len(sentence)
+        buf = ''
+        while x < N:
+            y = route[x][1] + 1
+            l_word = sentence[x:y]
+            if re_eng.match(l_word) and len(l_word) == 1:
+                buf += l_word
+                x = y
+            else:
+                if buf:
+                    yield buf
+                    buf = ''
+                yield l_word
+                x = y
+        if buf:
+            yield buf
+            buf = ''
+
+    def __cut_DAG(self, sentence):
+        DAG = self.get_DAG(sentence)
+        route = {}
+        self.calc(sentence, DAG, route)
+        x = 0
+        buf = ''
+        N = len(sentence)
+        while x < N:
+            y = route[x][1] + 1
+            l_word = sentence[x:y]
+            if y - x == 1:
+                buf += l_word
+            else:
+                if buf:
+                    if len(buf) == 1:
+                        yield buf
+                        buf = ''
+                    else:
+                        if not self.FREQ.get(buf):
+                            recognized = finalseg.cut(buf)
+                            for t in recognized:
+                                yield t
+                        else:
+                            for elem in buf:
+                                yield elem
+                        buf = ''
+                yield l_word
+            x = y
+
+        if buf:
+            if len(buf) == 1:
+                yield buf
+            elif not self.FREQ.get(buf):
+                recognized = finalseg.cut(buf)
+                for t in recognized:
+                    yield t
+            else:
+                for elem in buf:
+                    yield elem
+
+    def cut(self, sentence, cut_all=False, HMM=True):
+        '''
+        The main function that segments an entire sentence that contains
+        Chinese characters into seperated words.
+
+        Parameter:
+            - sentence: The str(unicode) to be segmented.
+            - cut_all: Model type. True for full pattern, False for accurate pattern.
+            - HMM: Whether to use the Hidden Markov Model.
+        '''
+        sentence = strdecode(sentence)
+
+        if cut_all:
+            re_han = re_han_cut_all
+            re_skip = re_skip_cut_all
+        else:
+            re_han = re_han_default
+            re_skip = re_skip_default
+        if cut_all:
+            cut_block = self.__cut_all
+        elif HMM:
+            cut_block = self.__cut_DAG
+        else:
+            cut_block = self.__cut_DAG_NO_HMM
+        blocks = re_han.split(sentence)
+        for blk in blocks:
+            if not blk:
+                continue
+            if re_han.match(blk):
+                for word in cut_block(blk):
+                    yield word
+            else:
+                tmp = re_skip.split(blk)
+                for x in tmp:
+                    if re_skip.match(x):
+                        yield x
+                    elif not cut_all:
+                        for xx in x:
+                            yield xx
+                    else:
+                        yield x
